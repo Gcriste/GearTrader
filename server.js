00d01b19ec70f6228db3306040805a7eb549e071
Griffin <griffincriste@gmail.com>
2020-06-21 21:12:07 -0500

    fixed server page

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-	// set static folder
 	app.use(express.static('client/build'));
-	app.get('*', function(req, res) {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
 }
 // Add routes, both API and view
 app.use(routes);
@@ -23,7 +22,7 @@ require('./config/passport')(passport);
 // require("./routes/api/users")(router)
 // Connect to the Mongo DB
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI);
 
 // Start the API server
 app.listen(PORT, function() {
